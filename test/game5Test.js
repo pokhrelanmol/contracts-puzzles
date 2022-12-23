d2bd12707e3082ea78f14b4901d9d6ebabc41461
anmol <anmolpokhrel46@gmail.com>
2022-12-23 19:42:28 +0530

    completed game 5 using brute force way

--- test/game5Test.js
+++ test/game5Test.js
@@ -1,21 +1,31 @@
-const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
-const { assert } = require('chai');
+const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
+const { assert } = require("chai");
+const { ethers } = require("hardhat");
 
-describe('Game5', function () {
-  async function deployContractAndSetVariables() {
-    const Game = await ethers.getContractFactory('Game5');
-    const game = await Game.deploy();
+describe("Game5", function () {
+    async function deployContractAndSetVariables() {
+        const Game = await ethers.getContractFactory("Game5");
+        const game = await Game.deploy();
+        const signers = await ethers.getSigners();
+        const threshold = 0x00ffffffffffffffffffffffffffffffffffffff;
+        let signer;
+        for (let _signer of signers) {
+            if (_signer.address < threshold) {
+                console.log("valid address: ", _signer.address);
+                signer = _signer;
+                break;
+            }
+        }
+        return { game, signer };
+    }
+    it("should be a winner", async function () {
+        const { game, signer } = await loadFixture(
+            deployContractAndSetVariables
+        );
 
-    return { game };
-  }
-  it('should be a winner', async function () {
-    const { game } = await loadFixture(deployContractAndSetVariables);
+        await game.connect(signer).win();
 
-    // good luck
-
-    await game.win();
-
-    // leave this assertion as-is
-    assert(await game.isWon(), 'You did not win the game');
-  });
+        // leave this assertion as-is
+        assert(await game.isWon(), "You did not win the game");
+    });
 });
